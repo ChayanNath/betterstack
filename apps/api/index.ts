44ed876f1232d5cf90e82377dc2ae47b8f6e8520
Chayan <chayannath19@gmail.com>
2025-06-01 23:02:18 +0530

    feat: Added status api implementation

--- apps/api/index.ts
+++ apps/api/index.ts
@@ -28,7 +28,28 @@ app.post("/api/v1/website", async (req, res) => {
   }
 });
 
-app.get('/api/v1/status/:id', (req, res) => { });
+app.get("/api/v1/status/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const website = await prismaClient.website.findUnique({
+      where: {
+        id,
+      },
+      include: {
+        ticks: true,
+      },
+    });
+    if (!website) {
+      res.status(404).json({ message: "Website not found" });
+      return;
+    }
+    res.status(200).json({ website });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Failed to connect to database" });
+    return;
+  }
+});
 
 app.listen(process.env.PORT || 3000, () => {
   console.log('Server is running on port 3000');
